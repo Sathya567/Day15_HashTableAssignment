4c3c70787a9957b488f1333942c124957aaf4c2f
BootCampBL <satyabhandari43@gmail.com>
2021-07-27 17:46:19 +0530

    [sathya] Added- Removing Word from Phrase

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Day15_HashTable
         {
             Console.WriteLine("Welcome to Hash Table Program");
 
-            //            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            //            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);              UC_1
             //            hash.Add("0", "to");
             //            hash.Add("1", "be");
             //            hash.Add("2", "or");
@@ -21,18 +21,37 @@ namespace Day15_HashTable
             //            Console.WriteLine("Frequency of or  : " + hash.GetFrequencyOfWords("or"));
             //            Console.WriteLine("Frequency of not : " + hash.GetFrequencyOfWords("not"));
 
+            //    string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            //    string[] para = paragraph.Split(" ");
+            //    MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
+            //    int key = 0;
+            //    foreach (string word in para)                                                                UC_2
+            //    {
+            //        hash.Add(key, word);
+            //        key++;
+            //    }
+            //    Frequency frequency = new Frequency();
+            //    frequency.Freqncy(hash);
+            //}
+
+            Console.WriteLine();
+
+
             string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
             string[] para = paragraph.Split(" ");
-            MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
-            int key = 0;
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(para.Length);
+            int keys = 0;
             foreach (string word in para)
             {
-                hash.Add(key, word);
-                key++;
+                int k = keys++;
+                string key = k.ToString();
+                hash.Add(word, key);
             }
-            Frequency frequency = new Frequency();
-            frequency.Freqncy(hash);
+            string value = hash.Get("Paranoids");
+            hash.GetArrayPosition(value);
+            hash.Remove("avoidable");
         }
     }
-    
 }
+
+    
